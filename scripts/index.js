3e30db394e8c228beb5934a9da13204b76f176c1
Qwebeck5 <Qwebeck5@gmail.com>
2017-02-26 21:17:03 +0300

    Another variant of homework

--- scripts/index.js
+++ scripts/index.js
@@ -2,27 +2,32 @@ var ball = document.querySelector(".discoball");
 
 function goDisco() {
 	//do your disco magic here !
-ball.setAttribute("style" , "background: linear-gradient(to left, #FF512F , #DD2476); ")
+ball.setAttribute("class" , "discoball "+ classes[i])
+	i++
+	if(i>=6) {i = 0}
 }
-function goToDisco(){
-	ball.setAttribute("style", "background: linear-gradient(to left, #E55D87 , #5FC3E4);")
-}
-function goOur(){
-	ball.setAttribute("style", "background: linear-gradient(to left, #3494E6 , #EC6EAD);")
-}
-function awesome(){
-	ball.setAttribute("style", "background: linear-gradient(to left, #3494E6 , #EC6EAD; ")
-}
-function disco(){
-	ball.setAttribute("style"," background: linear-gradient(to left, #00c3ff , #ffff1c)")
-}
-function finishDisco(){
-	ball.setAttribute("style","background: linear-gradient(to left, #de6161 , #2657eb);  ")
-}
+// function goToDisco(){
+// 	ball.setAttribute("style", "background: linear-gradient(to left, #E55D87 , #5FC3E4);")
+// }
+// function goOur(){
+// 	ball.setAttribute("style", "background: linear-gradient(to left, #3494E6 , #EC6EAD);")
+// }
+// function awesome(){
+// 	ball.setAttribute("style", "background: linear-gradient(to left, #3494E6 , #EC6EAD; ")
+// }
+// function disco(){
+// 	ball.setAttribute("style"," background: linear-gradient(to left, #00c3ff , #ffff1c)")
+// }
+// function finishDisco(){
+// 	ball.setAttribute("style","background: linear-gradient(to left, #de6161 , #2657eb);  ")
+// }
 
 setInterval(goDisco,300);
-setInterval(goToDisco,600)
-setInterval(goOur,900)
-setInterval(awesome,1200)
-setInterval(disco,1500)
-setInterval(finishDisco,1800);
+// setInterval(goToDisco,600)
+// setInterval(goOur,900)
+// setInterval(awesome,1200)
+// setInterval(disco,1500)
+// setInterval(finishDisco,1800);
+ 
+ var classes = ["here","goes","our" ,"awesome","disco","ball"]
+ var i = 0
